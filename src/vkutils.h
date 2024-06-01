d329c57f43961a33fa43aa6c2d53dc88c21b2092
Guillem Baldi <guillembaldi@gmail.com>
2024-06-01 07:20:11 +0200

    cleanup

--- src/vkutils.h
+++
@@ -1,7 +0,0 @@
-#include <vulkan/vulkan.h>
-
-namespace vkutils {
-uint32_t findMemoryType(VkPhysicalDevice physicalDevice, uint32_t typeFilter,
-                        VkMemoryPropertyFlags flags);
-
-}  // namespace vkutils
