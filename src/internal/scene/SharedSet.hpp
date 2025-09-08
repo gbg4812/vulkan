2b750867e9b4b2bb8953c22987ca7db66bb6d2d8
Guillem Baldi <guillembaldi@gmail.com>
2025-09-08 15:21:53 +0200

    wkp

--- src/internal/scene/SharedSet.hpp
+++ src/internal/scene/SharedSet.hpp
@@ -5,8 +5,9 @@ class SharedSet {
    private:
     class Node {
         std::shared_ptr<T> value;
-        Node* right;
-        Node* left;
+        Node* right = nullptr;
+        Node* left = nullptr;
+        Node* parent = nullptr;
     };
 
    private:
@@ -14,12 +15,49 @@ class SharedSet {
 
    public:
     class iterator {
+       public:
+        iterator(Node* current, Node* last) : current(current), last(last) {}
+
        private:
         Node* current;
+        Node* last = nullptr;
 
        public:
         std::shared_ptr<T> operator*() { return current->value; }
         std::shared_ptr<T> operator->() { return current->value; }
-        iterator& operator++() { current = current->right; };
+        iterator& operator++() {
+            if (last == current->parent) {
+                if (current->left != nullptr) {
+                    last = current;
+                    current = current->left;
+                    return ++(*this);
+                }
+                last = current;
+                return *this;
+
+            } else if (last == current) {
+                if (current->right != nullptr) {
+                    current = current->left;
+                }
+            } else if (last == current->left) {
+            } else if (last == current->right) {
+            }
+        }
+        iterator operator++(int) {
+            iterator aux = {current, last};
+            ++(*this);
+            return aux;
+        }
     };
+
+    iterator begin() {
+        Node* current = root;
+        Node* last = nullptr;
+        while (current->left != nullptr) {
+            last = current;
+            current = current->left;
+        }
+
+        return iterator(current, last);
+    }
 };
